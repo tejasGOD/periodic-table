306be992b7f909d2491cf87bd22f6f112e8d2217
tejasGOD <tejas212009@gmail.com>
2024-08-24 14:28:49 +0530

    h4

--- index.js
+++ index.js
@@ -1,30 +1,49 @@
 fetch("data.json")
   .then((response) => response.json())
   .then((elements) => {
-    const tableContainer = document.getElementById("periodic-table");
-    const numCols = 18; // Number of columns for the periodic table
-    tableContainer.style.gridTemplateColumns = `repeat(${numCols}, minmax(0, 1fr))`;
+    const mainTableContainer = document.getElementById("main");
+    mainTableContainer.style.gridTemplateColumns = `repeat(18, minmax(0, 1fr))`;
+    const fTableContainer = document.getElementById("f");
+    fTableContainer.style.gridTemplateColumns = `repeat(14, minmax(0, 1fr))`;
 
     elements.forEach((element) => {
-      const elementDiv = document.createElement("div");
-      elementDiv.classList.add("element");
-      elementDiv.style.backgroundColor = `#${element.cpkHexColor}`;
+      if (element.block !== "f") {
+        let elementDiv = document.createElement("div");
+        elementDiv.classList.add("🧪");
+        elementDiv.style.backgroundColor = `#${element.cpkHexColor}`;
 
-      // Calculate the position
-      const column = element.group;
-      const row = element.period;
+        // Calculate the position
+        const column = element.group;
+        const row = element.period;
 
-      elementDiv.style.gridColumn = column;
-      elementDiv.style.gridRow = row;
+        elementDiv.style.gridColumn = column;
+        elementDiv.style.gridRow = row;
 
-      elementDiv.innerHTML = `
+        elementDiv.innerHTML = `
                 ${element.symbol}<br>
                 ${element.atomicNumber}
             `;
-      tableContainer.appendChild(elementDiv);
+        mainTableContainer.appendChild(elementDiv);
+      } else {
+        let elementDiv = document.createElement("div");
+        elementDiv.classList.add("🧪");
+        elementDiv.style.backgroundColor = `#${element.cpkHexColor}`;
+        elementDiv.innerHTML = `
+                ${element.symbol}<br>
+                ${element.atomicNumber}
+            `;
+
+        // Handle the positioning of f-block elements
+        if (element.atomicNumber >= 57 && element.atomicNumber <= 71) {
+          elementDiv.style.gridColumn = (element.atomicNumber - 57).toString();
+          elementDiv.style.gridRow = "1";
+        } else if (element.atomicNumber >= 89 && element.atomicNumber <= 103) {
+          elementDiv.style.gridColumn = (element.atomicNumber - 89).toString();
+          elementDiv.style.gridRow = "2";
+        }
+
+        fTableContainer.appendChild(elementDiv);
+      }
     });
   })
   .catch((error) => console.error("Error fetching data:", error));
-
-
-// cleaned up everything and done with a responsive table
