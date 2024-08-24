248626597c3e6255fedd4a0e6090c316ed4e8313
tejasGOD <tejas212009@gmail.com>
2024-08-24 13:23:44 +0530

    Create style.js

---
+++ style.js
@@ -0,0 +1,22 @@
+body {
+    background-color: #F7FAFC;
+    color: #2D3748;
+}
+
+.element {
+    background-color: #E2E8F0;
+    padding: 0.5rem;
+    border-radius: 0.25rem;
+    text-align: center;
+    font-size: 0.75rem;
+    font-weight: bold;
+    transition: transform 0.2s;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.element:hover {
+    transform: scale(1.05);
+    background-color: #CBD5E0;
+}
