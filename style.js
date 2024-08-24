e4c526ea4ad07bed1a456d64af5a83418dbf33ed
tejasGOD <tejas212009@gmail.com>
2024-08-24 14:29:07 +0530

    h4

--- style.js
+++ style.js
@@ -1,22 +1,36 @@
 body {
-    background-color: #F7FAFC;
-    color: #2D3748;
+  /* background-color: #F7FAFC; */
+  background-color: #2d3748;
 }
 
-.element {
-    background-color: #E2E8F0;
-    padding: 0.5rem;
-    border-radius: 0.25rem;
-    text-align: center;
-    font-size: 0.75rem;
-    font-weight: bold;
-    transition: transform 0.2s;
-    display: flex;
-    justify-content: center;
-    align-items: center;
+.🧪 {
+  background-color: #e2e8f0;
+  padding: 0.5rem;
+  border-radius: 0.1rem;
+  text-align: center;
+  font-size: 0.75rem;
+  font-weight: bold;
+  transition: transform 0.1s;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 
-.element:hover {
-    transform: scale(1.05);
-    background-color: #CBD5E0;
+.🧪:hover {
+  transform: scale(1.25);
+  background-color: #cbd5e0;
+}
+.title {
+  font-family: "Matemasie", sans-serif;
+  font-weight: 400;
+  font-style: normal;
+  font-size: 4rem;
+  color: blanchedalmond;
+}
+.cont {
+  width: 100%;
+  overflow-x: scroll;
+}
+.ta {
+  min-width: 800px;
 }
